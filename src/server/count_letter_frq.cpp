07c459fe31f2d2d4e16fd0270171298bea76ae2f
Marta Reed <martaree@student.21-school.ru>
2024-04-25 20:23:19 +0700

    dev: count_letter_frequency() func implementation

--- src/server/count_letter_frq.cpp
+++ src/server/count_letter_frq.cpp
@@ -1,5 +1,60 @@
 #include "count_letter_frq.h"
 
+static std::string get_table_header(std::string msg);
+static std::string get_table_data(const char letter, int count);
+static bool is_msg_has_letters(std::string msg);
+
 std::string count_letter_frequency(std::string str) {
-  return str;
+  std::map<char, int> letters_dict;
+  std::map<char, int>::iterator iter;
+
+  std::string result = get_table_header(str);
+  if (!is_msg_has_letters(str)) {
+    return result;
+  }
+  
+  for(int i = 0; i < str.length(); i++) {
+    if (isalpha(str[i])) {
+      letters_dict[str[i]]++;
+    }
+  }
+
+  for(iter = letters_dict.begin(); iter != letters_dict.end(); iter++) {
+    std::string column = get_table_data(iter->first, iter->second);
+    result += column;
+  }
+
+  return result;
+}
+
+std::string get_table_header(std::string msg) {
+  const char *message = "Message";
+  const char *sep = "|";
+
+  char buff[BUFSIZ] = {0};
+  sprintf(buff, "%-8s %s %s\n", message, sep, msg.c_str());
+
+  std::string result = buff;
+  return result;
+}
+
+std::string get_table_data(const char letter, int count) {
+  char buff[BUFSIZ] = {0};
+
+  const char *sep = "|";
+  sprintf(buff, "%-8c %s %d\n", letter, sep, count);
+
+   std::string result = buff;
+   return result;
+}
+
+bool is_msg_has_letters(std::string msg) {
+  bool result = false;
+  for(char& c : msg) {
+    if (isalpha(c)) {
+      result = true;
+      break;
+    }
+  }
+  return result;
 }
--- src/server/count_letter_frq.h
+++ src/server/count_letter_frq.h
@@ -2,6 +2,8 @@
 #define SRC_COUNT_LETTER_FRQ_H_
 
 #include <string>
+#include <map>
+#include <iostream>
 
 std::string count_letter_frequency(std::string str);
 
